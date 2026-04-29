fd8bba6b360b671587d4bb9ec595d9a890ac1954
Tjunior6crom2006-afk <junior6crom2006@gmail.com>
2026-04-29 12:07:22 +0200

    update: fix render config + main

--- main.py
+++ main.py
@@ -1,46 +1,52 @@
 import logging
-from os import name as os_name, system as os_system
+import threading
+import os
+from flask import Flask
+
 from huepy import bold, green
 from functions.variables import API_ID, API_HASH, BOT_TOKEN, NAME_BOT
 from pyrogram import Client
 from pyrogram.enums import ParseMode
-import json
-from functools import wraps
 from watcher import start_watcher_in_thread
-import os
-
-# CLEAR CONSOLE
-if os_name == "nt": os_system("cls") 
-else: os_system("clear")
 
 # LOGS
-
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.StreamHandler()
-    ]
+    format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
+# FLASK APP (para Render)
+web_app = Flask(__name__)
 
-# CONFIGURE CLIENT
+@web_app.route("/")
+def home():
+    return "Bot activo ✅"
+
+def run_web():
+    port = int(os.environ.get("PORT", 8080))
+    web_app.run(host="0.0.0.0", port=port)
+
+# BOT
 app = Client(
     NAME_BOT,
-    api_id =  API_ID,
-    api_hash = API_HASH,
-    bot_token = BOT_TOKEN,
-    plugins = dict(root="plugins"),
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins"),
     parse_mode=ParseMode.HTML
 )
 
-if __name__ == "__main__":
+def run_bot():
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    
     watcher = start_watcher_in_thread(current_directory)
-    
+
+    print(bold(green("Iniciando Zyrex Checker 1.0 Beta 「♚」")))
+
     try:
-        print(bold(green(f"Iniciando Zyrex Checker 1.0 Beta 「♚」")))
         app.run()
     except KeyboardInterrupt:
         watcher.stop()
+
+if __name__ == "__main__":
+    threading.Thread(target=run_web).start()
+    run_bot()
